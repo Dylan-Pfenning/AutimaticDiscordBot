8f283aceae5fcbe2eeec262afe98bf2772463d73
Dylan-Pfenning <dylan.pfenning@gmail.com>
2020-10-19 23:37:05 -0700

    Instead of just trusting the user. I shifted to a sudo 2FA system where the user gets a phrase sent to them and has to post it in twitch chat for verification on the discord.

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const Discord = require('discord.js');
+const tmi = require('tmi.js');
 const fetch = require('node-fetch');
 global.Headers = global.Headers || require("fetch-headers");
 const { prefix, token, twitchID, twitchSecret, TimTwitchID } = require('./config.json');
 const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
+const opts = {
+    identity: {
+        username: "TimBot",
+        password: twitchSecret
+    },
+    channels: [
+        "autimatictv"
+    ]
+};
+const twitchNameMap = new Map();
 
+const twitchClient = new tmi.client(opts);
 
 client.once('ready', async ready => {
-    console.log('GPQ Bot 1.0 Ready!');
+    console.log('TimBot Up and Running!');
+    twitchClient.connect();
+    console.log('Tim bot in the chat beep boop')
 })
 
 client.on('message', async message => {
     if (message.channel instanceof Discord.DMChannel) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-
     if (command === 'verify') {
         //grab the user ID from the name given to verify
         const twitchName = message.content.split(" ");
@@ -25,33 +38,38 @@ client.on('message', async message => {
             }
         }).then(response => response.json());
         //Get the userID
-        if(userInfo.data.length == 0){
+        if (userInfo.data.length == 0) {
             message.channel.send("This twitch user doesn't exist.");
-            return;   
+            return;
         }
-        userID = userInfo.data[0].id;
-        //checks to see if the user actually follows auti
-        let checkFollow = await fetch(`https://api.twitch.tv/helix/users/follows?to_id=${TimTwitchID}&from_id=${userID}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${twitchSecret}`,
-                'Client-ID': twitchID
-            }
-        }).then(response => response.json());
+        //Generate sentance to say to verify identity
+        let secretPhrase = 'Tim is the best awper in NA'
+        twitchNameMap.set(`${twitchName[1]}`, {key: secretPhrase, discord: message});
+        message.member.send(`Your secret phrase to type in autimatics chat for verification is;`);
+        message.member.send(secretPhrase);
+    }
+});
 
-        if (checkFollow.total == 1) {
-            message.channel.send("Approved");
+
+twitchClient.on('message', (target, context, msg) => {
+    const secretPhrase = msg.trim();
+    if (twitchNameMap.has(context.username)) {
+        if (secretPhrase === twitchNameMap.get(context.username).key) {
+            let discord = twitchNameMap.get(context.username).discord;
             //assign roles to the user
-            const role = message.guild.roles.cache.find(role =>role.name === "Follower");
-            message.member.roles.add(role);
+            const role = discord.guild.roles.cache.find(role => role.name === "Follower");
+            discord.member.roles.add(role);
             //change users name
-            message.member.setNickname(userInfo.data[0].display_name);
-        }
-        else {
-            message.channel.send("Please follow autimatic to access his discord!");
+            discord.member.setNickname(context.username);
+            discord.channel.send(`Verified <@${discord.member.id}>`);
+            //Remove this value from the map
+            twitchNameMap.delete(context.username);
         }
     }
-
 });
 
 client.login(token);
+
+
+//to refresh https://twitchtokengenerator.com/api/refresh/<REFRESH_TOKEN>
+
